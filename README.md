75234b755b2ae6bb88f0fa6c77324bd5fc48b5ba
miguelchaveinte <miguelchaveinte23@gmail.com>
2022-07-07 11:09:01 +0200

    Add README.md

---
+++ README.md
@@ -0,0 +1,10 @@
+# appDeepint
+
+Este repositorio es una mini-prueba para aplicar TypeScript y probar fetch. Se ha utilizado la API (https://jsonplaceholder.typicode.com/api) para obtener datos de una API REST.
+
+## Commandos
+
+Empiece ejecutando `npm install` dentro de la carpeta del proyecto.
+`npm run tsc` para contruir el proyecto,
+`npm run start` para contruir el proyecto,
+`npm run dev` para correrlo en modo desarrollo.
--- build/index.js
+++ build/index.js
@@ -4,11 +4,14 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const posts_routes_1 = __importDefault(require("./routes/posts_routes"));
 const app = (0, express_1.default)();
 app.use(express_1.default.json()); // for parsing application/json
 const PORT = 3000;
+// use the routes provided by the posts module
+app.use('/posts', posts_routes_1.default);
 app.get('/', (_, res) => {
-    res.send('Hello World');
+    res.send('Probando typescript y fetch');
 });
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
